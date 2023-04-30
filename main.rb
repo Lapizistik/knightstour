52fb93028c0360f0d9087d07fc13964bdee6206a
Lapizistik <github@istik.de>
2023-04-30 17:12:11 +0000

    Sort the knight_moves by angle

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'matrix'
 
 class ChessBoard
   attr_reader :size
-  def initialize(n: 8, m: n, v: Vector[2,1])
+  def initialize(n: 8, m: n, v: Vector[2, 1])
     @data = Matrix.zero(n, m)
     @moves = compute_knight_moves(v)
     @size = n * m
@@ -11,12 +11,12 @@ class ChessBoard
   # get the content of a specific board cell
   # We define the method [], which means we can do
   # board[pos] and get the cell value
-  def [](pos)   
+  def [](pos)
     # we will call this with a position
     # but matrix expects two values, so we decompose it
     @data[*pos]
   end
-  
+
   # set one board cell
   # b[pos] = 3
   def []=(pos, value)
@@ -30,49 +30,52 @@ class ChessBoard
 
   # is the given position a valid move?
   def valid?(pos)
-    pos[0] >= 0 && pos[1] >= 0 && @data[*pos] == 0 
+    pos[0] >= 0 && pos[1] >= 0 && @data[*pos] == 0
   end
-  
+
   # give a nice string representation of a board
-  def to_s(cell_width=3)
-    @data.row_vectors.map { |v|   # '|' ?
-      v.to_a.map { |x| x.to_s.rjust(cell_width) }.join 
-    }.join("\n")
+  def to_s(cell_width = 3)
+    @data.row_vectors.map do |v| # '|' ?
+      v.to_a.map { |x| x.to_s.rjust(cell_width) }.join
+    end.join("\n")
   end
 
   # The following part is completely overengineered ;-)
   # We should simply have given the possible moves ;-)
-  Rot90 = Matrix[[0,-1],[1,0]]
-  Flip  = Matrix[[0,1],[1,0]]
+  Rot90 = Matrix[[0, -1], [1, 0]]
+  Flip  = Matrix[[0, 1], [1, 0]]
   def compute_knight_moves(v)
     # v is a vector with two components
     vt = Flip * v
-    (0..3).map { |i| Rot90**i * v } +
-        (0..3).map { |i| Rot90**i * vt }  
+    # we have them counter clockwise
+    (0..3).flat_map { |i| r = Rot90**i and [r * v, r * vt] }
   end
-end # class 
+end # class
 
 class Runner
-  def initialize(board: ChessBoard.new)
+  def initialize(board: ChessBoard.new, roundtrip: false)
     @board = board
+    @roundtrip = roundtrip
   end
-  def run(pos: Vector[0,0], k: 1, moves: @board.possible_moves(pos))
+
+  def run(pos: Vector[0, 0], k: 1, moves: @board.possible_moves(pos))
     # First we tell the chessboard that we are here:
     @board[pos] = k
 
-    return @board if k >= @board.size # found it!
+    return @board if (k >= @board.size) &&
+                     !@roundtrip || moves.any? { |p| @board[p] == 1 }
 
     # so lets decide the next step
-    moves.map { |p| [p, @board.possible_moves(p)] }.sort_by { |p, ms| ms.length }
-      .each do |p, ms| 
-        if result = run(pos: p, k: k + 1, moves: ms)
-          return result
-        end
+    moves.map { |p| [p, @board.possible_moves(p)] }.sort_by { |_p, ms| ms.length }
+         .each do |p, ms|
+      if result = run(pos: p, k: k + 1, moves: ms)
+        return result
       end
+    end
     @board[pos] = 0
-    return false
+    false
   end
 end
 
-#puts Runner.new(board: ChessBoard.new(n: 8)).run
-puts Runner.new.run
+# puts Runner.new(board: ChessBoard.new(n: 8)).run
+puts Runner.new(roundtrip: true).run
